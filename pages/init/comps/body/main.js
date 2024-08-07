4605d3904f54e104d0daf8b611008061d7adf0bc
Philippe <philippe.idalgoprestes@gmail.com>
2024-08-07 17:53:13 -0300

    initBody done

--- pages/init/comps/body/main.js
+++ pages/init/comps/body/main.js
@@ -1,5 +1,177 @@
 export default function bodyDiv(){
     return(`
-        <div>teste</div>
+        <div class="initBody">
+            <div class="initBodySection">
+                <div class="initBodySectionTitle">
+                    Conteúdos mais vistos
+                </div>
+                <div class="initBodySectionScroll">
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="initBodySection">
+                <div class="initBodySectionTitle">
+                    Conteúdos mais vistos
+                </div>
+                <div class="initBodySectionScroll">
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="initBodySection">
+                <div class="initBodySectionTitle">
+                    Conteúdos mais vistos
+                </div>
+                <div class="initBodySectionScroll">
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                    <div>
+                        <div class="initBodySectionItem">
+                            material 1
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     `)
 }
